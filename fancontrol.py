f56096cd448bb5b43361d5e8a76e10e57b938d9b
Morten Guldager <mogul@mogul.dk>
2021-04-30 13:16:53 +0200

    1

---
+++ fancontrol.py
@@ -0,0 +1,43 @@
+import glob
+import gpiozero
+import time
+import re
+
+class DS18S20:
+    def __init__(self):
+        base_dir = '/sys/bus/w1/devices/'
+        device_folder = glob.glob(base_dir + '28*')[0]
+        self.dev_file = device_folder + '/w1_slave'
+    
+    def get(self):
+        while True:
+            with open(self.dev_file, 'r') as F:
+                lines = F.readlines() 
+            if "YES" not in lines[0]:
+                time.sleep(0.2)
+                continue
+            m = re.search(r" t=(\d+)$", lines[1])
+            if m:
+                return int(m.group(1)) / 1000
+            Exception("boom")
+
+def transform(val, off, fact, min_, max_):
+    x = (val - off) * fact
+    x = min(x, max_)
+    x = max(x, min_)
+    return x
+
+sensor = DS18S20()
+fan = gpiozero.PWMOutputDevice(pin=18)
+while True:
+    cpu_temp = gpiozero.CPUTemperature().temperature
+    hdd_temp = sensor.get()
+    cpu_need = transform(cpu_temp, 35, 0.1, 0, 1)
+    hdd_need = transform(hdd_temp, 25, 0.1, 0, 1)
+    speed = max([cpu_need, hdd_need])
+     
+    print(cpu_temp, cpu_need, hdd_temp, hdd_need, speed)
+
+    fan.value = speed
+    
+    time.sleep(10)
